953df351efc0918f3981cdac9e23b7011f422d8c
JBasias <jbasias@gmail.com>
2023-12-12 18:34:46 -0500

    a needed commit

--- classes/student.js
+++ classes/student.js
@@ -2,6 +2,34 @@ const Person = require('./person');
 
 // Your code here
 
+class Student extends Person{
+
+  constructor(firstName, lastName, age, GPA, major) {
+    super(firstName, lastName, age);
+    this.GPA = GPA;
+    this.major = major; // This line is moved after calling super()
+  }
+
+  static compareGPA(S1, S2)
+  {
+    //console.log(S1.major);
+    if(S1.GPA === S2.GPA)
+    {
+          console.log("Both students have the same GPA.");
+    }
+    else if(S1.GPA > S2.GPA)
+    {
+      console.log(` ${S1.firstName} ${S1.lastName} has the higher GPA.`);
+    }
+    else
+    {
+      console.log(` ${S2.firstName} ${S2.lastName} has the higher GPA.`);
+    }
+  }
+
+}
+
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- classes/teacher.js
+++ classes/teacher.js
@@ -1,5 +1,27 @@
 const Person = require('./person');
 
+class Teacher extends Person{
+
+  constructor(firstName, lastName, age, yearsOfExperience, subject) {
+    super(firstName, lastName, age);
+    this.yearsOfExperience = yearsOfExperience; // This line is moved after calling super()
+    this.subject = subject;
+  }
+
+  static combinedYearsOfExperience(Teach)
+  {
+     let ret=0;
+     Teach.forEach(ele => {
+      ret += ele.yearsOfExperience;
+     });
+
+     return(ret);
+
+  }
+
+}
+
+
 // Your code here
 
 /****************************************************************************/
